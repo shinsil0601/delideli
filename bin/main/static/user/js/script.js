685b9ee9d5db2c2a482ad4d20085798afe8aebc8
DESKTOP-CBAV1V6\ksna0 <ksna0303@gmail.com>
2024-08-07 17:40:32 +0900

    Update .gitignore to exclude bin directory

--- bin/main/static/user/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    // header category dropdown
-    $('.header__bot-menu > li').hover(
-        function() {
-            $(this).find('ul').stop(true, true).fadeToggle('slow');
-        }
-    );
-});
