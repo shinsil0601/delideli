d5bf76dac23a7d454bd55ba5a5f04ae8a6d30d5f
DESKTOP-MST6EOC\ksna0303 <ksna0303@gmail.com>
2024-08-17 23:18:29 +0900

    feat / Archive 사용자 마이페이지

--- src/main/resources/static/user/js/script.js
+++ src/main/resources/static/user/js/script.js
@@ -50,4 +50,21 @@ $(document).ready(function(){
             prevEl: ".mainEvent__prevNext--prev",
         },
     });
+
+    // MyPage Point CountUp
+    $('.js-point').each(function() {
+        $(this).prop("Counter", 0).animate(
+            {
+                Counter: $(this).text(),
+            },
+            {
+                duration: 4000,
+                easing: "swing",
+                step: function(now) {
+                    now = Number(Math.ceil(now)).toLocaleString('en');
+                    $(this).text(now);
+                },
+            },
+        );
+    });
 });
