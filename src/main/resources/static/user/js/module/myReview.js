bcf5091588b0e25ce3e56fbbaa4277aae6ee47dc
DESKTOP-CBAV1V6\ksna0 <ksna0303@gmail.com>
2024-08-19 17:06:10 +0900

    feat / Archive 사용자 - 리뷰목록, 리뷰작성

--- src/main/resources/static/user/js/module/myReview.js
+++ src/main/resources/static/user/js/module/myReview.js
@@ -1,9 +1,13 @@
 function showReviewList(type) {
     if (type === 'available') {
-        document.getElementById('available-review-list').style.display = 'block';
+        $('.myReview__btn-con--1').addClass('active');
+        $('.myReview__btn-con--2').removeClass('active');
+        document.getElementById('available-review-list').style.display = 'grid';
         document.getElementById('written-review-list').style.display = 'none';
     } else {
+        $('.myReview__btn-con--2').addClass('active');
+        $('.myReview__btn-con--1').removeClass('active');
         document.getElementById('available-review-list').style.display = 'none';
-        document.getElementById('written-review-list').style.display = 'block';
+        document.getElementById('written-review-list').style.display = 'grid';
     }
 }
